36a93ceef60ecdce5e513766deaf9f0546e09c24
ugarebahubali10-create <ugarebahubali10@gmail.com>
2026-03-26 12:26:10 +0530

    config.py

--- config.py
+++ config.py
@@ -3,28 +3,28 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "34741124"))
+API_HASH = os.environ.get("API_HASH", "78fc38a63c927a42ffb829c31fad046c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8094194589:AAFqLz445AmhBwbmLqDebW3iJ6iHB1f6Ph4")
+ADMIN = int(os.environ.get("ADMIN", "7937361794"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQISG4QAoNGmVWHfPDLk6sJ1jJdarAB4H8ACISTIC9bcIGWt_q1qGrIEVMqOCEsTWGWjSGAskN3rKc6--huLZXsG4Jt7d3NnHqwLC44V2eIhJod1eSrFOsPKsuGQUIKAKbEFlphauo0SXf1TJo41XiXYTu5KH_1CMAhaEM8ucSn31tXwTzZLPHeYgjfUpLPV9dt06OoGIcNFTl15aMG7laoSqSfs1-RtXELmvl-QO9Z5s8B0z94bPOP2lj3ZL7unmJnk_XdtUM0DhBOt23cmAvj9bpXGXpgD1uoThBNM74AyDv9vTFGPoYGUzVpG1Ty_Gnf7FFyMFz5lhuMxruAK9NvlOD6UEgAAAAHZGoeCAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Ballu_With_Movies")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1003523047118"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "Url mongodb+srv://godballuyt:GoD_Ballu_Yt@1234#1234@cluster0.eko6oc3.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "godballuyt")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/f0c394d3555d6bf9149aa-68e0d4c77aec20da90.jpg")
 
 
 
